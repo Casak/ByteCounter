4a1a49169d280e9f5d07d854d6a1e66fbe6d17b5
Casak123 <casak123@gmail.com>
2016-10-10 21:46:22 +0300

    Initial commit

---
+++ app/src/main/java/ru/casak/bytecounter/ByteCounterActivity.java
@@ -0,0 +1,14 @@
+package ru.casak.bytecounter;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+
+public class ByteCounterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_byte_counter);
+    }
+}
