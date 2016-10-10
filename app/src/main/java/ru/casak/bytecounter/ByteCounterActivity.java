23494fa91f1765862a76f9c8ba1df4a449add05d
Casak123 <casak123@gmail.com>
2016-10-11 00:48:19 +0300

    Implement the task

--- app/src/main/java/ru/casak/bytecounter/ByteCounterActivity.java
+++ app/src/main/java/ru/casak/bytecounter/ByteCounterActivity.java
@@ -1,14 +1,84 @@
 package ru.casak.bytecounter;
 
+import android.os.AsyncTask;
+import android.os.CountDownTimer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import org.apache.commons.net.ftp.FTP;
+import org.apache.commons.net.ftp.FTPClient;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.math.BigInteger;
 
 
 public class ByteCounterActivity extends AppCompatActivity {
 
+    private final static String TAG = ByteCounterActivity.class.getSimpleName();
+    private final static String HOSTNAME = "speedtest.tele2.net";
+    private final static String FILENAME = "1MB.zip";
+    private final static Integer TIMER_MINUTES = 1;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_byte_counter);
+        new MyTask().execute();
+    }
+
+    class MyTask extends AsyncTask<Void, Void, Integer> {
+        private final String TAG = MyTask.class.getSimpleName();
+        BigInteger counter = new BigInteger("0");
+
+        @Override
+        protected void onPreExecute() {
+            new CountDownTimer(TIMER_MINUTES * 60 * 1000, 1000) {
+
+                public void onTick(long millisUntilFinished) {
+                    Log.d(TAG, "seconds remaining: " + millisUntilFinished / 1000);
+                }
+
+                public void onFinish() {
+                    onPostExecute(0);
+                    System.exit(0);
+                }
+            }.start();
+        }
+
+        @Override
+        protected Integer doInBackground(Void... params) {
+            FTPClient client = new FTPClient();
+            try {
+                while (true) {
+                    client.connect(HOSTNAME);
+                    client.enterLocalPassiveMode();
+                    client.setFileType(FTP.BINARY_FILE_TYPE);
+                    client.login("anonymous", "anonymous");
+                    String remoteFile = "/" + FILENAME;
+
+                    InputStream inputStream = client.retrieveFileStream(remoteFile);
+                    byte[] bytesArray = new byte[4096];
+                    int bytesRead = -1;
+                    if (inputStream == null) {
+                        System.out.println("could not retrieve file: " + FILENAME);
+                        continue;
+                    }
+                    while ((bytesRead = inputStream.read(bytesArray)) != -1) {
+                        counter = counter.add(new BigInteger(bytesRead + ""));
+                    }
+                    inputStream.close();
+                    client.disconnect();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Integer param) {
+            Log.d(TAG, "Total bytes: " + counter.toString());
+        }
     }
 }
